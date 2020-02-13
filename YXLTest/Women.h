93e432f231c61d092b500c9bbb67f62e8cc86543
袁鑫亮 <306619887@qq.com>
2020-02-13 15:37:49 +0800

    c2

--- YXLTest/Women.h
+++ YXLTest/Women.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Women : NSObject
 
+- (void)shopping;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- YXLTest/Women.m
+++ YXLTest/Women.m
@@ -10,4 +10,8 @@
 
 @implementation Women
 
+- (void)shopping {
+    NSLog(@"购物");
+}
+
 @end
