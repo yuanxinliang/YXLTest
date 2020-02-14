0d9740859d32233fb8d7780a307f3109f87b5e17
yuanxinliang <306619887@qq.com>
2020-02-14 15:40:06 +0800

    c

---
+++ YXLTest/Bird.h
@@ -0,0 +1,17 @@
+//
+//  Bird.h
+//  YXLTest
+//
+//  Created by jdapple on 2020/2/14.
+//  Copyright © 2020 XL Yuen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Bird : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YXLTest/Bird.m
@@ -0,0 +1,13 @@
+//
+//  Bird.m
+//  YXLTest
+//
+//  Created by jdapple on 2020/2/14.
+//  Copyright © 2020 XL Yuen. All rights reserved.
+//
+
+#import "Bird.h"
+
+@implementation Bird
+
+@end
