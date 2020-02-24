654488ee2ed9289d01925edca12155583dfff6ea
yuanxinliang <306619887@qq.com>
2020-02-24 21:31:49 +0800

    提交 1

--- YXLTest/AppDelegate.m
+++ YXLTest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"修改 1....");
     return YES;
 }
 
